6ed3a8d8610f9a7d673ad8f5780ac20a3835e13d
Camilo <camilo.inuneko@gmail.com>
2021-05-31 21:57:01 -0400

    actualizacion

--- src/Componentes/Capsula_Flotacion.js
+++ src/Componentes/Capsula_Flotacion.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import Panel from './Propiedades';
+
 //
 
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useDrag } from 'react-dnd'
+import Celda from './Componentes/casilla';
+import Arrow_down from './Componentes/Arrow_down';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Capsula from './Componentes/Capsula_Flotacion';
-import Panel from './Componentes/Propiedades';
-ReactDOM.render(<Capsula/>,
-  document.getElementById('root')
+
+ReactDOM.render(<><Celda num='1'/></>,
+  document.getElementById('main')
 );
+
+ReactDOM.render(<><Arrow_down /><Arrow_down/></>,document.getElementById('tool'));
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vital
