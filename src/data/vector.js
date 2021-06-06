b1d843ffe203e502af5e6ef920779e5968a45f3a
franciscocastro54 <francisco.castro.init@gmail.com>
2021-06-06 00:35:40 -0400

    Vector return {nombre,propiedades}

--- src/data/vector.js
+++ src/data/vector.js
@@ -1,3 +1,4 @@
+/*
 function Vector(densidadP1=0,porcSolido1=0,ley1=0,caudalP1=0){
 
     const densidadP= densidadP1;
@@ -10,14 +11,41 @@ function Vector(densidadP1=0,porcSolido1=0,ley1=0,caudalP1=0){
     const calMSolido = () => (calMPulpa()*porcSolido)
     const calFino = () => (ley*calMSolido())
     
-   const MPulpa= calMPulpa();
+    const MPulpa= calMPulpa();
     const MSolido= calMSolido();
     const Fino= calFino();
-const data= [MPulpa,MSolido,Fino]
+    const data= [MPulpa,MSolido,Fino]
 
-return data;
+    return data;
     //this.MostrarDatos= function(){   return [this.calMPulpa,this.calMSolido,this.calFino]   }
 
+}
+*/
+function Vector(nombre1="nn",densidadP1=0,porcSolido1=0,ley1=0,caudalP1=0){
+    
+    const densidadP= densidadP1;
+    const porcSolido=porcSolido1;
+    const ley=ley1;
+    const caudalP=caudalP1;
+    
+    const calMPulpa = ()=>(caudalP*densidadP)    
+    const calMSolido = () => (calMPulpa()*porcSolido)
+    const calFino = () => (ley*calMSolido()) 
+
+    const MPulpa1= calMPulpa();
+    const MSolido1= calMSolido();
+    const Fino1= calFino();
+
+    return  {nombre:nombre1
+            ,propiedades:
+            {densidad:densidadP1
+            ,porcSolido:porcSolido1
+            ,ley:ley1
+            ,caudalP:caudalP1
+            ,MPulpa:MPulpa1
+            ,MSolido:MSolido1
+            ,Fino:Fino1}
+        }
 }
 
 export default Vector;
