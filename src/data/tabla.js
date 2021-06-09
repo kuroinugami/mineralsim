e1269e2b3883b46fef129ae53011b5335a36e69e
franciscocastro54 <francisco.castro.init@gmail.com>
2021-06-09 15:20:14 -0400

    dataTable

--- src/data/tabla.js
+++ src/data/tabla.js
@@ -1,5 +1,99 @@
 import React from 'react'
 import Truncate from '../data/Truncado';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import DataTable from 'react-data-table-component'
+
+
+const vectores=[{"nombre":"vector1"            
+,"densidad":123
+,"porcSolido":1
+,"ley":1
+,"caudalP":2
+,"MPulpa":3
+,"MSolido":4
+,"Fino":5
+,"tipo":"tipo1"
+},
+{"nombre":"vector2"           
+,"densidad":123
+,"porcSolido":1
+,"ley":2
+,"caudalP":2
+,"MPulpa":3
+,"MSolido":4
+,"Fino":2
+,"tipo":"tipo1"
+}]
+
+const columnas=[
+    {
+        name: 'ID',
+        selector: 'nombre',
+        sortable: 'true'
+    },
+    {
+        name: 'Densidad(ton/m3)',
+        selector: 'densidad',
+        sortable: 'true'
+    },
+    {
+        name: 'Porcentaje de sólido(%)',
+        selector: 'porcSolido',
+        sortable: 'true'
+    },
+    {
+        name: 'Ley(%)',
+        selector: 'ley',
+        sortable: 'true'
+    },
+    {
+        name: 'Caudal(m3/hr)',
+        selector: 'caudalP',
+        sortable: 'true'
+    },
+    {
+        name: 'Masa pulpa(ton/hr)',
+        selector: 'MPulpa',
+        sortable: 'true'
+    },
+    {
+        name: 'Masa solido(ton/hr)',
+        selector: 'MSolido',
+        sortable: 'true'
+    },
+    {
+        name: 'Fino(ton/hr)',
+        selector: 'Fino',
+        sortable: 'true'
+    }
+]
+
+const paginacionOpciones={
+    rowsPerPageText: 'Filas por página',
+    rangeSeparatorText: 'de',
+    selectAllRowsItem: true,
+    selectAllRowsItemText: 'Todos'
+}
+
+function TableVector(){
+    return(
+        <div className="table-responsive">
+            <DataTable 
+                columns={columnas}
+                data={vectores}
+                title="Flujos ingresados"
+                pagination
+                paginationComponentOptions={paginacionOpciones}
+                fixedHeader
+                fixedHeaderScrollHeight="600px"
+            />
+        </div>
+    )
+}
+
+
+
+/*
 
 function viewVector(vector){
     return(
@@ -14,6 +108,13 @@ function viewVector(vector){
             <td>{Truncate(vector.Fino)}</td>
         </tr>
     )
+}
+
+function listarV(listarV) {
+    for (const vector in listaVector) {
+        viewVector(vector)
+    }
+
 }
 
 function tablaVector(...listaVector){
@@ -31,9 +132,9 @@ function tablaVector(...listaVector){
                 <th>Masa solido(ton/hr)</th>
                 <th>Fino(ton/hr)</th>
                 <script>
-                    for (const vector in listaVector) {
-                        viewVector(vector)
-                    }
+                    {% for  vector in listaVector %} 
+                        <>viewVector(vector)</>
+                    {%endfor%}
                 </script>
             </thead>
             
@@ -41,7 +142,7 @@ function tablaVector(...listaVector){
         </>
     )
 }
-/*
+
 function tablaBalance(listaVectores) {
     
 }
@@ -49,3 +150,4 @@ function tablaBalance(listaVectores) {
 */
 export default tablaVector
     
+
