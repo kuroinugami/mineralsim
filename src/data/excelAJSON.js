21d5f1e682cd1132b83065e9ee68872ad788847f
Camilo <camilo.inuneko@gmail.com>
2021-07-16 18:52:19 -0400

    Updates

--- src/data/excelAJSON.js
+++
@@ -1,9 +0,0 @@
-let XLSX = require("xlsx")
-
-const ExcelAJSON=()=>{
-    const excel = XLSX.readFile("")
-    var nombreHoja=excel.SheetNames
-    let datos = XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[0]])
-    console.log(datos)
-}
-ExcelAJSON()
